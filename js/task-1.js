427610bd39897b1850167532599d210310284731
Oleksii Marushchak <98462083+OleksiiMarushchak@users.noreply.github.com>
2025-10-26 15:18:29 +0000

    update tast-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -4,7 +4,7 @@ console.log(`Number of categories: ${ctgr.length}`);
 
 ctgr.forEach(category => {
     const title = category.querySelector('h2').textContent;
-    const itemsCount = category.querySelectorAll('li').length - 1; 
+    const itemsCount = category.querySelectorAll('li').length; 
     console.log(`Category: ${title}`);
     console.log(`Elements: ${itemsCount}`);
 });
